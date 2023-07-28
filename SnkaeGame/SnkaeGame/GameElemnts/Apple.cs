afd9e2dce1f62aee4f2ebba93bbe59e95b105f8e
Mmk208 <137777715+Mmk208@users.noreply.github.com>
2023-07-28 11:40:07 +0200

    Add files via upload

---
+++ SnkaeGame/SnkaeGame/GameElemnts/Apple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SnakeGame.GameElemnts
+{
+    class Apple:GameIntity
+    {
+        public Apple(int size)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = size;
+            rect.Height = size;
+            rect.Fill = Brushes.Red;
+            UIElement = rect;
+
+        }
+
+    }
+}
---
+++ SnkaeGame/SnkaeGame/GameElemnts/Snake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SnakeGame.GameElemnts
+{
+    class Snake:GameIntity
+    {
+        public Snake(int size)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = size;
+            rect.Height = size;
+            rect.Fill = Brushes.Green;
+            UIElement = rect;
+
+        }
+
+        public bool IsHead { get; set; }
+    }
+}
